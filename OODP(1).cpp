642de56482ffe7470dfc67a53b2c91a8dc39bd30
Sailesh092004 <113753265+Sailesh092004@users.noreply.github.com>
2023-04-08 22:43:04 +0530

    Add files via upload

---
+++ OODP(1).cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class AddressBook {
+public:
+    void addContact(string name, string phoneNumber, string email) {
+        contacts[name] = make_pair(phoneNumber, email);
+    }
+
+    pair<string, string> getContact(string name) {
+        if (contacts.count(name) == 0) {
+            return make_pair("", "");
+        }
+        return contacts[name];
+    }
+
+private:
+    map<string, pair<string, string>> contacts;
+};
+
+int main() {
+    AddressBook book;
+
+    // prompt the user to add contacts to the address book
+    while (true) {
+        cout << "Enter a name (or 'quit' to exit): ";
+        string name;
+        getline(cin, name);
+
+        if (name == "quit") {
+            break;
+        }
+
+        cout << "Enter " << name << "'s phone number: ";
+        string phoneNumber;
+        getline(cin, phoneNumber);
+
+        cout << "Enter " << name << "'s email address: ";
+        string email;
+        getline(cin, email);
+
+        book.addContact(name, phoneNumber, email);
+    }
+
+    // prompt the user to retrieve contacts from the address book
+    while (true) {
+        cout << "Enter a name to retrieve contact information (or 'quit' to exit): ";
+        string name;
+        getline(cin, name);
+
+        if (name == "quit") {
+            break;
+        }
+
+        pair<string, string> contact = book.getContact(name);
+        if (contact.first.empty() && contact.second.empty()) {
+            cout << name << " not found in address book." << endl;
+        } else {
+            cout << name << "'s phone number: " << contact.first << endl;
+            cout << name << "'s email address: " << contact.second << endl;
+        }
+    }
+
+    return 0;
+}
